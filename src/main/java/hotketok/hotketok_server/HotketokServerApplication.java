a9386a6016365d61a3b871eaba29993f5fcfd23d
shinminkyoung1 <dottiee99@naver.com>
2024-11-19 15:44:59 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/hotketok/hotketok_server/HotketokServerApplication.java
@@ -0,0 +1,13 @@
+package hotketok.hotketok_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotketokServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotketokServerApplication.class, args);
+	}
+
+}
