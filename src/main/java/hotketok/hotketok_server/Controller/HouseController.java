f628a1564744ce7b28a5d8016b16ab62f36870c2
shinminkyoung1 <dottiee99@naver.com>
2024-12-03 10:07:11 +0900

    refactor: 주소 기능 구현 수정

--- src/main/java/hotketok/hotketok_server/Controller/HouseController.java
+++ src/main/java/hotketok/hotketok_server/Controller/HouseController.java
@@ -1,33 +1,27 @@
 package hotketok.hotketok_server.Controller;
 
+import hotketok.hotketok_server.DTO.AddressResponse;
 import hotketok.hotketok_server.Domain.House;
+import hotketok.hotketok_server.Service.AddressService;
 import hotketok.hotketok_server.Service.HouseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 public class HouseController {
 
-    private final HouseService houseService;
+    private final AddressService addressService;
 
-    @PostMapping("/address")
-    public ResponseEntity<House> saveAddress(@RequestBody AddressRequest addressRequest) {
-        House house = houseService.saveAddress(addressRequest.getAddress());
-        return ResponseEntity.ok(house);
+    @GetMapping("/house/search")
+    public ResponseEntity<AddressResponse> searchAddress(@RequestParam String keyword) {
+
+        AddressResponse response = addressService.searchAddress(keyword);
+        return ResponseEntity.ok(response);
     }
 
-    public static class AddressRequest {
-        private String address;
-
-        public String getAddress() {
-            return address;
-        }
-
-        public void setAddress(String address) {
-            this.address = address;
-        }
-    }
 }
 
