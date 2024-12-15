f1a469a71a4944f0f1ff3fd646a1f7558b941a5f
shinminkyoung1 <dottiee99@naver.com>
2024-12-15 21:00:42 +0900

    feat: 웹페이지 요청서 조회 기능 구현

--- src/main/java/hotketok/hotketok_server/Controller/VendorWebController.java
+++ src/main/java/hotketok/hotketok_server/Controller/VendorWebController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Map;
@@ -19,6 +20,7 @@ public class VendorWebController {
     private final VendorWebService vendorWebService;
 
     // 수리 요청 조회
+    @GetMapping("/vendor/search")
     public ResponseEntity<List<Map<String, Object>>> getServiceRequest() {
 
         List<Map<String, Object>> response = vendorWebService.getServiceRequest();
--- src/main/java/hotketok/hotketok_server/Service/VendorWebService.java
+++ src/main/java/hotketok/hotketok_server/Service/VendorWebService.java
@@ -1,10 +1,13 @@
 package hotketok.hotketok_server.Service;
 
+import hotketok.hotketok_server.Domain.ConstructionDate;
 import hotketok.hotketok_server.Domain.ServiceRequest;
 import hotketok.hotketok_server.Repository.ServiceRequestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,8 +18,32 @@ public class VendorWebService {
     private final ServiceRequestRepository serviceRequestRepository;
 
     // 수리 요청 조회
-//    public List<Map<String, Object>> getServiceRequest() {
-//
-//        // 완료되지 않은 요청서 조회
-//    }
+    public List<Map<String, Object>> getServiceRequest() {
+
+        // 완료되지 않은 요청서 조회
+        List<ServiceRequest> serviceRequests = serviceRequestRepository.findByStatus(0);
+
+        List<Map<String, Object>> response = new ArrayList<>();
+
+        for (ServiceRequest request : serviceRequests) {
+            Map<String, Object> requestMap = new HashMap<>();
+            requestMap.put("request_id", request.getRequestId());
+            requestMap.put("category", request.getCategory());
+            requestMap.put("created_at", request.getCreatedAt().toString());
+            requestMap.put("address", request.getHouseUserMapping().getHouse().getHouseAddress());
+            requestMap.put("request_image", request.getRequestImage());
+            requestMap.put("request_description", request.getRequestDescription());
+
+            // 스케줄 데이터를 key: 날짜, value: 시간으로 설정
+            Map<String, List<String>> scheduleMap = new HashMap<>();
+            for (ConstructionDate schedule : request.getConstructionDates()) {
+                scheduleMap.put(schedule.getDate().toString(), schedule.getTimes());
+            }
+            requestMap.put("request_schedule", scheduleMap);
+
+            response.add(requestMap);
+        }
+
+        return response;
+    }
 }
