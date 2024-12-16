b6b54fc631902a29beabf4845705eb6d552551c8
SHIN__0.0 <ajtwoddltom@gmail.com>
2024-12-16 22:21:22 +0900

    fix/houseUserMappingRepo

--- src/main/java/hotketok/hotketok_server/Repository/HouseUserMappingRepository.java
+++ src/main/java/hotketok/hotketok_server/Repository/HouseUserMappingRepository.java
@@ -4,6 +4,8 @@ import hotketok.hotketok_server.Domain.House;
 import hotketok.hotketok_server.Domain.HouseUserMapping;
 import hotketok.hotketok_server.Domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +18,8 @@ public interface HouseUserMappingRepository extends JpaRepository<HouseUserMappi
 
     List<HouseUserMapping> findAllByHouse(House house);
 
-    Optional<User> findUserByUnitNumber(String unitNumber);
+    @Query("SELECT hum.user FROM HouseUserMapping hum WHERE hum.unitNumber = :unitNumber")
+    Optional<User> findUserByUnitNumber(@Param("unitNumber") String unitNumber);
+
 
 }
